cb91febcc719bae4ec6f4373e653013c87cc237a
Susu-spec <suleimansuwaibat@gmail.com>
2025-03-06 11:54:43 +0100

    modify styling in css files

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,13 +32,13 @@ module.exports = {
           },
           fontSize: {
               'headingLg': ['2rem', { lineHeight: '35.2px' }],
-              'title': ['3.75rem'],
+              'title': ['3.75rem', { lineHeight: '100%' }],
               'titleMd': ['5rem'],
               'titleSmall': ['4rem', { lineHeight: '4.25rem'}],
               'titleLarge': ['6.5rem', { lineHeight: '6.25rem'}],
               'heading': ['2.25rem'],
               'bodyLg': ['1.25rem', { lineHeight: '2.5rem'}],
-              'bodyMd': ['1.125rem'],
+              'bodyMd': ['1.125rem', { lineHeight: '160%'}],
               'bodyReg': ['1rem', { lineHeight: '25.6px'}]
           },
           fontWeight: {
